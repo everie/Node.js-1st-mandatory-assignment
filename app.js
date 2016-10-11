8c2ba4a8193a4f3f68b4b0d8c0c4900b3d8e1a10
Hans <imorgenerdetmandag@gmail.com>
2016-10-11 11:21:31 +0200

    the best commit

---
+++ app.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Hans on 11-10-2016.
+ */
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+
+    fs.readFile('./data/index.json', 'utf8', function(err, data) {
+        res.render('index', JSON.parse(data));
+    });
+
+});
+
+app.get('/about', function(req, res) {
+
+    fs.readFile('./data/about.json', 'utf8', function(err, data) {
+        res.render('index', JSON.parse(data));
+    });
+
+});
+
+app.get('*', function(req, res) {
+    res.redirect('/');
+});
+
+app.listen(3000);
